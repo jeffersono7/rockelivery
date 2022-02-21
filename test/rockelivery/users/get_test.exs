6c36a39532243795433389d3428f55ef908173c4
Jefferson Farias <jefferson.farias7@gmail.com>
2022-02-21 08:01:06 -0300

    get user test

---
+++ test/rockelivery/users/get_test.exs
@@ -0,0 +1,31 @@
+defmodule Rockelivery.Users.GetTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Ecto.UUID
+  alias Rockelivery.Error
+  alias Rockelivery.Users.Get
+
+  describe "by_id/1" do
+    test "when user not exists, returns an error not found" do
+      actual =
+        UUID.generate()
+        |> Get.by_id()
+
+      expected = {:error, Error.build_user_not_found_error()}
+
+      assert actual == expected
+    end
+
+    test "when user exists, return it" do
+      user = build(:user)
+
+      actual = Get.by_id(user.id)
+
+      expected = {:ok, user}
+
+      assert actual = expected
+    end
+  end
+end
