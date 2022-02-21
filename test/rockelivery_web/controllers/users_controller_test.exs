f60c80bdeba9e55cfa904f452b97d7988ec66221
Jefferson Farias <jefferson.farias7@gmail.com>
2022-02-21 08:58:08 -0300

    users controller test

--- test/rockelivery_web/controllers/users_controller_test.exs
+++ test/rockelivery_web/controllers/users_controller_test.exs
@@ -3,6 +3,7 @@ defmodule RockeliveryWeb.UsersControllerWeb do
 
   import Rockelivery.Factory
 
+  alias Ecto.UUID
   alias Rockelivery.User
 
   describe "create/2" do
@@ -57,7 +58,58 @@ defmodule RockeliveryWeb.UsersControllerWeb do
         |> delete(Routes.users_path(conn, :delete, id))
         |> response(:no_content)
 
-        assert response == ""
+      assert response == ""
+    end
+  end
+
+  describe "show/2" do
+    test "when there is a user with the given id, returns it", %{conn: conn} do
+      %User{id: id} = insert(:user)
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :show, id))
+        |> json_response(:ok)
+
+      expected = %{
+        "user" => %{
+          "address" => "Rua do Elixir",
+          "age" => 27,
+          "cpf" => "95495487980",
+          "email" => "email-do-elixir@com",
+          "id" => "86339c4e-b2a8-4aa7-954c-9f27ff23f1e1"
+        }
+      }
+
+      assert response == expected
+    end
+
+    test "when there is not user with the given id, returns not found error", %{conn: conn} do
+      id = UUID.generate()
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :show, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => _} = response
+    end
+  end
+
+  describe "update/2" do
+    test "given there is a user, when update it with other params, return it updated", %{
+      conn: conn
+    } do
+      user = insert(:user)
+
+      params = %{age: 55}
+
+      response =
+        conn
+        |> put(Routes.users_path(conn, :update, user.id), params)
+        |> json_response(:ok)
+
+      assert %{"user" => %{"age" => 55}} = response
     end
   end
 end
