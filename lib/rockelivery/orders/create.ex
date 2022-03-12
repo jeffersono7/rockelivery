7bb6cb1598870821b6dd4bd8c273271a2e64c56c
Jefferson Farias <jefferson.farias7@gmail.com>
2022-03-12 11:29:57 -0300

    create order

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -1,12 +1,12 @@
 defmodule Rockelivery.Orders.Create do
-
   import Ecto.Query
 
-  alias Rockelivery.{Item, Order, Repo}
+  alias Rockelivery.{Error, Item, Order, Repo}
 
   def call(params) do
     params
     |> fetch_items()
+    |> handle_items(params)
   end
 
   defp fetch_items(%{"items" => items_param} = params) do
@@ -16,12 +16,38 @@ defmodule Rockelivery.Orders.Create do
 
     query
     |> Repo.all()
-    |> validate_items(item_ids)
+    |> validate_and_multiply_items(item_ids, params)
   end
 
-  defp validate_items(items, items_ids) do
+  defp validate_and_multiply_items(items, items_ids, items_params) do
     items_map = Map.new(items, fn item -> {item.id, item} end)
 
-    Enum.all?(items_ids, fn id -> Map.has_key?(items_map, id) end)
+    valid? = Enum.all?(items_ids, fn id -> Map.has_key?(items_map, id) end)
+
+    multiply_items(valid?, items_map, items_params)
   end
+
+  defp multiply_items(false, _items, _items_params), do: {:error, "Invalid ids!"}
+
+  defp multiply_items(true, items, items_params) do
+    items = Enum.reduce(items_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
+      item = Map.get(items, id)
+
+      acc ++ List.duplicate(item, quantity)
+    end)
+
+    {:ok, items}
+  end
+
+  defp handle_items({:error, result}, _params), do: {:error, Error.build(:bad_request, result)}
+
+  defp handle_items({:ok, items}, params) do
+    params
+    |> Order.changeset(items)
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Order{} = order}), do: order
+  defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
 end
