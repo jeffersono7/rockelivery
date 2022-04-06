86b603cf0dea043cac15e77f37bebafefb0ba55a
Jefferson Farias <jefferson.farias7@gmail.com>
2022-04-06 08:36:25 -0300

    authenticate and generate token

--- lib/rockelivery_web/auth/guardian.ex
+++ lib/rockelivery_web/auth/guardian.ex
@@ -1,7 +1,7 @@
 defmodule RockeliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rockelivery
 
-  alias Rockelivery.User
+  alias Rockelivery.{Error, User}
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
@@ -10,4 +10,21 @@ defmodule RockeliveryWeb.Auth.Guardian do
     |> Map.get("sub")
     |> Rockelivery.get_user_by_id()
   end
+
+  def authenticate(%{"id" => user_id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- Rockelivery.get_user_by_id(user_id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false ->
+        {:error, Error.build(:unauthorized, "Please verify your credentials")}
+
+      error ->
+        Pbkdf2.no_user_verify()
+        error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing params")}
 end
