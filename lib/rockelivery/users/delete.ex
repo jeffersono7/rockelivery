7b77c116d405d2d2831a68e6eea3745478b2424e
Jefferson Farias <jefferson.farias7@gmail.com>
2022-04-21 18:54:08 -0300

    fix credo

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -1,7 +1,7 @@
 defmodule Rockelivery.Users.Delete do
   alias Rockelivery.{Error, Repo, User}
 
-  @spec call(binary()) :: {:error, %Error{}} | {:ok, %User{}}
+  @spec call(binary()) :: {:error, Error} | {:ok, User}
   def call(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
